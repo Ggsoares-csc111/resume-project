15cfc4f5e75d3f2a81148a1cda21161f343cb94a
Ggsoares-csc111 <purpleshell11@gmail.com>
2025-09-17 09:18:31 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,12 @@
+// Define Varibles
+let myName = "Gabriella";
+let title = "Student";
+let experience = "Social Media, Canva, and Marketing";
+let education = "Oral Roberts University"
+//let skills = ["Customer service", "Commuication", "Leadership"]
+
+// Connect Varibles to the HTML (ligaments)
+document.getElementById("myName").textContent = myName;
+document. getElementById ("title").textContent = title;
+document. getElementById ("experience").textContent = experience;
+document. getElementById ("education").textContent = education;
